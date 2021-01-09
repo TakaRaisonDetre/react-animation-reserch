d84820de01a65adbb43a38f3adacfc8bddd05c73
原貴悌 <taka_raison@genkiteinoiMac.local>
2021-01-09 17:27:48 +0900

    panResponder Event handler check

--- src/DeckStack.js
+++ src/DeckStack.js
@@ -5,7 +5,19 @@ import {View, Text, Animated, PanResponder, Dimensions} from 'react-native'
 
 class DeckStack extends Component {
 
+constructor(props){
+    super(props);
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder :() =>true,
+      onPanResponderMove: (event, gesture)=>{
+          console.log(gesture)
+      },
+      onPanResponderRelease: ()=>{}
+    });
 
+    //this.panResponder = panResponder;
+    this.state = {panResponder};
+}
 
 
 renderCards(){
